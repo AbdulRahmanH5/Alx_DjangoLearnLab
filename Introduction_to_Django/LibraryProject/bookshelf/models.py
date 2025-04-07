a783c1ae87e1de3881ff25e0c6d42b531d72515f
AbdulRahmanH5 <AbdulRahmanHilal134@gmail.com>
2025-04-08 01:55:55 +0200

    permissions

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -23,9 +23,18 @@ class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
-
 # Create your models here.
 class Book (models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ('can_view', 'Can view book'),
+            ('can_edit', 'Can edit book'),
+            ('can_delete', 'Can delete book'),
+            ('can_create', 'Can create book'),
+        ]
+    def __str__(self):
+        return self.title
