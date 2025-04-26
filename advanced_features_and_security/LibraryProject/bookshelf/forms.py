a603d7455d24067bce64c1632c3367be532b0a8d
AbdulRahmanH5 <AbdulrahmanHilal134@gmail.com>
2025-04-26 14:31:17 +0300

    Alx task2

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,7 @@ class BookForm(forms.Form):
     title = forms.CharField(max_length=100, required=True)
     author = forms.CharField(max_length=100, required=True)
     publication_year = forms.IntegerField(required=True)
+
+class ExampleForm(forms.Form):
+    example_field = forms.CharField(max_length=100, required=True)
+    another_field = forms.IntegerField(required=True)
